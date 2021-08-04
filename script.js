e8e757495f17c3d08e25337e22c98daf7460c663
Augusto Neves <augusto.agt1995@gmail.com>
2021-08-04 00:01:27 -0300

    Initial Commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function displayAbout(){
+    const arrow = document.getElementById('animation');
+    arrow.classList.toggle('rotate');
+
+    const display = document.getElementById('about');
+    display.classList.toggle('active')
+}
+
